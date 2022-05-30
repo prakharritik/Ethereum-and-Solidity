6be760e60bb0a0ce549b6d63bd8791f646396f75
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-05-30 19:52:25 +0530

    Add code for inbox.

---
+++ Inbox/compile.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf-8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    // Each Solidity source file to be compiled must be specified by defining either
+    // a URL to the file or the literal file content.
+    // See https://docs.soliditylang.org/en/v0.8.5/using-the-compiler.html#input-description
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    metadata: {
+      useLiteralContent: true,
+    },
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+module.exports = output.contracts["Inbox.sol"].Inbox;
